2c6510a95fbaf5a94bae85e87df22b29bdf9b75f
SalvianX <92953006+SalvianX@users.noreply.github.com>
2023-03-15 07:19:12 -0600

    Update Operacao.java

--- polimorfismo/polimorfismo/src/Operacao/Operacao.java
+++ polimorfismo/polimorfismo/src/Operacao/Operacao.java
@@ -1,5 +1,8 @@
 package Operacao;
-
+import java.util.List;
 public class Operacao {
-
+  public abstract void calcular(float num1, float num2);
+  public abstract void calcular(float num1, float num2, float num3);
+  public abstract void calcular(List<Float> numeros);
+  
 }
